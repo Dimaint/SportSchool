6a438fc33a433e968d4bc6d4272f494901e64b10
mObscene <s_aint@list.ru>
2020-09-13 17:38:21 +0600

    change database to Postgresql

--- school/school/settings.py
+++ school/school/settings.py
@@ -170,6 +170,14 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'school_db',
+        'USER': 'sport_school',
+        'PASSWORD': '123456',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
+    }
 }
 
 
